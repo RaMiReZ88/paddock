d5c37c4bb89146f15b0124ad58dab540506d639d
Amir <digiev.amir@glonassmobile.com>
2021-05-06 04:18:07 +0300

    init

---
+++ src/main/java/com/kolkhoz/paddock/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.kolkhoz.paddock.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
---
+++ src/test/java/com/kolkhoz/paddock/PaddockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolkhoz.paddock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
